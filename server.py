d2c1ec8ddd06309b45fba1216a453c83c2cdf0e6
Momeng <hqjshr123@gmail.com>
2019-08-03 22:34:23 +0800

    WSGI demo

--- server.py
+++ server.py
@@ -1,20 +1,103 @@
+import io
 import socket
+import sys
 
-s = socket.socket()
-s.bind(("127.0.0.1",3000))
-s.listen(5)
 
-while True:
-    connection, address = s.accept()
-    request_data = connection.recv(1024)
-    print("Request:")
-    print(request_data.decode('utf-8'))
-    print("__________________________")
-    print(address)
-    response = b"""\
-HTTP/1.1 200 OK
+class WSGIServer(object):
 
-Hello, World!
-"""
-    connection.sendall(response)
-    connection.close()
+    def __init__(self, server_address):
+        self.listen_socket = listen_socket = socket.socket()
+        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # 绑定地址，从配置中导入
+        listen_socket.bind(server_address)
+        listen_socket.listen(5)
+        host, port = self.listen_socket.getsockname()[:2]
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+        self.headers_set = []
+
+    def set_app(self, application):
+        self.application = application
+
+    def serve_forever(self):
+        listen_socket = self.listen_socket
+        while True:
+            self.client_connection, client_address = listen_socket.accept()
+            self.handle_one_request()
+
+    def handle_one_request(self):
+        request_data = self.client_connection.recv(1024)
+        self.request_data = request_data = request_data.decode('utf-8')
+        self.parse_request(request_data)
+
+        env = self.get_environ()
+
+        result = self.application(env, self.start_response)
+
+        self.finish_response(result)
+
+    def parse_request(self, text):
+        request_line = text.splitlines()[0]
+        request_line = request_line.rstrip('\r\n')
+        self.request_method, self.path, self.request_version = request_line.split()
+
+    def get_environ(self):
+        # 用字典接一下environ
+        env = {}
+        # WSGI协议规定应实现的参数
+        env['wsgi.version']      = (1, 0)
+        env['wsgi.url_scheme']   = 'http'
+        env['wsgi.input']        = io.StringIO(self.request_data)
+        env['wsgi.errors']       = sys.stderr
+        env['wsgi.multithread']  = False
+        env['wsgi.multiprocess'] = False
+        env['wsgi.run_once']     = False
+        # CGI协议规定应实现的参数（从http请求中解析得出）
+        env['REQUEST_METHOD']    = self.request_method
+        env['PATH_INFO']         = self.path
+        env['SERVER_NAME']       = self.server_name
+        env['SERVER_PORT']       = str(self.server_port)
+        return env
+
+    def start_response(self, status, response_headers, exc_info=None):
+        server_headers = [
+            ('Server', 'A simple webserver'),
+        ]
+        self.headers_set = [status, response_headers + server_headers]
+
+    def finish_response(self, result):
+        try:
+            status, response_headers = self.headers_set
+            response = f'HTTP/1.1 {status}\r\n'
+            for header in response_headers:
+                response += '{0}: {1}\r\n'.format(*header)
+            response += '\r\n'
+            for data in result:
+                response += data.decode('utf-8')
+            response_bytes = response.encode()
+            self.client_connection.sendall(response_bytes)
+        finally:
+            self.client_connection.close()
+
+
+def make_server(server_address, application):
+    server = WSGIServer(server_address)
+    server.set_app(application)
+    return server
+
+
+settings ={"HOST":"127.0.0.1",
+           "PORT": 8080,
+           }
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit('Provide a WSGI application object as module:callable')
+    app_path = sys.argv[1]
+    module, application = app_path.split(':')
+    module = __import__(module)
+    application = getattr(module, application)
+    SERVER_ADDRESS = (settings.get("HOST",""),settings.get("PORT",80))
+    httpd = make_server(SERVER_ADDRESS, application)
+    print(f'WSGIServer: Serving HTTP on  {SERVER_ADDRESS} ...\n')
+    httpd.serve_forever()
